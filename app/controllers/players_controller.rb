443d2211f6b7cc6edd351ac4f49c713c02443457
Chazz Gagliano <cgags17285@gmail.com>
2023-04-01 15:12:14 -0400

    CRUD created

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -7,5 +7,35 @@ class PlayersController < ApplicationController
     @player = Player.find_by(id: params["id"])
     render :show
   end
-  
+
+  def create
+    @player = Player.new(
+    name: params[:name],
+    jersey: params[:jersey],
+    team: params[:team],
+    position: params[:position]
+    )
+    @player.save
+    render :show
+  end
+
+  def update
+    @player = Player.find_by(id: params["id"])
+    @player.name = params[:name] || @player.name
+    @player.jersey = params[:jersey] || @player.jersey
+    @player.team = params[:team] || @player.team
+    @player.position = params[:position] || @player.position
+    @player.save
+    render :show
+  end
+
+  def destroy
+    @player = Player.find_by(id: params["id"])
+    @player.destroy
+    render json:{message: "Player Destroyed."}
+  end
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get"/players" => "players#index"
   get"/players/:id" => "players#show"
+  post"/players" => "players#create"
+  patch"/players/:id" => "players#update"
+  delete"/players/:id" => "players#destroy"
 end
