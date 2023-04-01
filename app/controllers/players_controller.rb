9089dcca5d304db6904e17c34feeb58526ae1070
Chazz Gagliano <cgags17285@gmail.com>
2023-04-01 14:51:48 -0400

    add index action

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,11 @@
+class PlayersController < ApplicationController
+  def index 
+    @players = Player.all
+    render :index
+  end
+  def show
+    @player = Player.find_by(id: params["id"])
+    render :show
+  end
+  
+end
