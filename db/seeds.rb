6e6e59599146e76a2f04c7f1a76fa7295732421c
Chazz Gagliano <cgags17285@gmail.com>
2023-04-08 14:19:04 -0400

    seed dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Player.create!([
+  {name: "aaron judge", jersey: 99, team: "yankees", position: "right field", mvp: 1},
+  {name: "bryce harper", jersey: 3, team: "phillies", position: "right field", mvp: 2},
+  {name: "juan soto", jersey: 22, team: "padres", position: "right field", mvp: 0}
+])
